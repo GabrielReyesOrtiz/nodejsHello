a79be14d8d7458130c4d36c296d8d8e1e8effa5a
GabrielReyesOrtiz <greyes17@ucol.mx>
2022-02-04 18:22:49 -0600

    Hello+Chalk

---
+++ index.js
@@ -0,0 +1,6 @@
+import chalk from 'chalk';  //Importamos el modulo Chalk que nos ayuda a personalizar nuestros textos.
+
+console.log(chalk.blue("Hello mundo")); // Imprimimos un texto para porbar como funciona Chalk.
+
+
+// corremos en codigo en la terminal con node index y sale nuestro texto con el color que le pedimos. 
